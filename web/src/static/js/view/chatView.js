183869e7c49db4bdd38352b5b2268d918b7cb373
JaniHarkonen <jani.harkonen.real@gmail.com>
2025-04-26 19:42:56 +0300

    Added chat functionality to the client UI with sending and receiving messages.

--- web/src/static/js/view/chatView.js
+++ web/src/static/js/view/chatView.js
@@ -6,27 +6,54 @@ export default function ChatView(props) {
 
   function receiveMessage(e) {
     console.log(e.data);
+    const messageElement = document.createElement("div");
+    messageElement.innerHTML = (`
+      ${e.data}
+    `);
+    const chatElement = document.getElementById("chat-content-messages");
+    chatElement.appendChild(messageElement);
   }
 
-  function sendMessage(messge) {
-    socket.send(message);
+  function sendMessage(e) {
+    e.preventDefault();
+    const messageInput = Object.values(e.target)[0];
+    socket.send(messageInput.value);
+    messageInput.value = "";
   }
 
   function connectToChatroom() {
     socket = new WebSocket(getChatRoute());
+    socket.onopen = () => {
+      const caption = document.getElementById("chat-caption-h1");
+      caption.innerHTML = "Connected to chatroom";
+    };
+    socket.onclose = (e) => {
+      const caption = document.getElementById("chat-caption-h1");
+      console.log(e)
+      caption.innerHTML = "Connection lost! Reason: " + e.code;
+    };
     socket.onmessage = receiveMessage;
   }
 
   const html = () => {
-    return (
-`
-  <div id="${props.id}" class="w-100 h-100 d-flex d-flex-justify-content-center d-flex-align-items-center">
-    <div class="w-50 h-50">
-      <h1>Your username is ${props.username}</h1>
-    </div>
-  </div>
-`
-    );
+    return (`
+      <div id="${props.id}" class="w-100 h-100 d-flex d-flex-justify-content-center d-flex-align-items-center">
+        <div class="w-50 h-100">
+          <div class="chat-content-grid h-100 w-100">
+            <h1 id="chat-caption-h1">Connecting to <code>${getChatRoute()}</code> as "${props.username}"</h1>
+            <div id="chat-content-messages" class="overflow-y-auto">
+
+            </div>
+            <div id="chat-content-input" class="padding-16px">
+              <form onsubmit="sendMessage(event)" class="w-100 d-flex gap-8px">
+                <input class="w-100" type="text"></input>
+                <button>Send</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    `);
   };
 
   return {
@@ -35,7 +62,7 @@ export default function ChatView(props) {
       console.log("mounted");
       connectToChatroom();
     },
-    scripts: []
+    scripts: [sendMessage]
   };
 }
 
--- web/src/static/js/view/loginView.js
+++ web/src/static/js/view/loginView.js
@@ -14,21 +14,19 @@ export default function LoginView(props) {
   }
 
   const html = () => {
-    return (
-`
-  <div id="${props.id}" class="w-100 h-100 d-flex d-flex-justify-content-center d-flex-align-items-center">
-    <div class="w-50 h-50">
-      <h1>Login</h1>
-      <form onsubmit="onLogin(event)">
-        <label class="w-100" for="login-username-input">Username:</label>
-        <input class="w-100" id="login-username-input" type="text"></input>
-        <!--<label class="w-100" for="login-password-input">Password:</label>
-        <input class="w-100" id="login-password-input" type="password"></input>-->
-      </form>
-    </div>
-  </div>
-`
-    );
+    return (`
+      <div id="${props.id}" class="w-100 h-100 d-flex d-flex-justify-content-center d-flex-align-items-center">
+        <div class="w-50 h-50">
+          <h1>Login</h1>
+          <form onsubmit="onLogin(event)">
+            <label class="w-100" for="login-username-input">Username:</label>
+            <input class="w-100" id="login-username-input" type="text"></input>
+            <!--<label class="w-100" for="login-password-input">Password:</label>
+            <input class="w-100" id="login-password-input" type="password"></input>-->
+          </form>
+        </div>
+      </div>
+    `);
   };
 
   return {
